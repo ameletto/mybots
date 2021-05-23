69f797119d405aa7359ed1f7a81bf3a24e743ed6
ameletto <meamymli@gmail.com>
2021-05-23 13:10:16 -0400

    Reading fitness and preparing for evolution.

--- solution.py
+++ solution.py
@@ -16,6 +16,10 @@ class SOLUTION:
         self.Create_Body()
         self.Create_Brain()
         os.system("python3 simulate.py")
+        fitnessFile = open("fitness.txt", "r")
+        self.fitness = float(fitnessFile.read())
+        print(self.fitness)
+        fitnessFile.close()
 
     def Create_World(self):
         pyrosim.Start_SDF("world.sdf")
