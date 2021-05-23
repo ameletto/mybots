c8fdc32243829bfd25042fd34b9c861180d603cc
ameletto <meamymli@gmail.com>
2021-05-23 14:12:40 -0400

    Mutating self.child.

--- solution.py
+++ solution.py
@@ -1,6 +1,7 @@
 import numpy
 import pyrosim.pyrosim as pyrosim
 import os
+import random
 
 length=1
 width=1
@@ -49,3 +50,8 @@ class SOLUTION:
             for currentColumn in range (0, 2):
                 pyrosim.Send_Synapse( sourceNeuronName = currentRow , targetNeuronName = currentColumn+3 , weight = self.weights[currentRow][currentColumn] )
         pyrosim.End()
+
+    def Mutate(self):
+        randomRow = random.randint(0, 2)
+        randomColumn = random.randint(0, 1)
+        self.weights[randomRow,randomColumn] = random.random() * 2 - 1
